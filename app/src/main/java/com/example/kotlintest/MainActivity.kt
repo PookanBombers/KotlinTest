0b2e28b9ec1c1f0d7e36d988504d296ba1cbfcfc
torte <tortellko@gmail.com>
2022-07-06 18:21:09 +0300

    HomeWorkOne | скопировал обьект класса и вывел его через лог

--- app/src/main/java/com/example/kotlintest/MainActivity.kt
+++ app/src/main/java/com/example/kotlintest/MainActivity.kt
@@ -4,6 +4,8 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import com.example.kotlintest.R
 import kotlin.math.log
@@ -16,6 +18,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        findViewById<Button>(R.id.button).setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+
+            }
+
+        })
+
+        val cat = object {
+            val name = "Barsik"
+        }
 
         //var - изменяемая
         //val - не изменяемая
@@ -35,6 +47,11 @@ class MainActivity : AppCompatActivity() {
 
         Log.d("@@@", noteTest.speed.toString())
         Log.d("@@@", noteTest.distance.toString())
+
+        val dataClass1 = NoteTest(50,200)
+        val dataClass2 = dataClass1.copy(30,150)
+
+        Log.d("@@@", dataClass2.toString())
 
     }
 }
